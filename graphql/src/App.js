7b3682b6f4e213dd1c109007ab80514318e1a792
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2022-09-26 11:39:25 +0200

    App

--- graphql/src/App.js
+++ graphql/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello, Dude!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://studio.apollographql.com/public/github/home?variant=current"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          👋 Welcome to the GitHub GraphQL API 🐙
         </a>
       </header>
     </div>
   );
 }
-
-export default App;
