e6ed47be2507ce6e74094af2c2dba573e782b095
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2022-10-31 17:35:43 +0100

    Something

--- apollo-graphql/src/components/Progressbar/index.js
+++ apollo-graphql/src/components/Progressbar/index.js
@@ -3,7 +3,13 @@ import {forwardRef} from 'react';
 
 const Progressbar = forwardRef((props, ref) => {
 	return (
-		<div ref={ref} role="progressbar" className="progressbar" {...props}>
+		<div
+			ref={ref}
+			role="progressbar"
+			aria-label="Please wait"
+			className="progressbar"
+			{...props}
+		>
 			Please wait...
 		</div>
 	);
